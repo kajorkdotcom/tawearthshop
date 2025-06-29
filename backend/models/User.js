5835a72b59d7a0e309668879d08ece1a4d4f0a8a
Jockkiez <92196787+kajorkdotcom@users.noreply.github.com>
2025-06-29 19:38:32 +0700

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
   username: String,
   password: String,
+  role: user,
   point: { type: Number, default: 100 },
   isAdmin: { type: Boolean, default: false }
 });
